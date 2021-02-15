f22f77f84798374eec4efb62433c7c3ce05f7f25
dxyjxy <37294186+dejanajan@users.noreply.github.com>
2021-02-15 18:18:09 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/css/style.css">
+    <script src="https://kit.fontawesome.com/06153639ea.js" crossorigin="anonymous"></script>
+    <title>Resume - D.Jankovic</title>
+  </head>
+  <body id="top">
+    <div class="main-container">
+      <div class="wrapper">
+          <header></header>
+          <main>
+            <div class="band">
+              <div class="container">
+                <div class="avatar">
+                  <img src="/assets/img/avatar1.jpeg" alt="personal-image">
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="personal-information">
+                      <h1>Dejana Janković</h1>
+                      <span>WEB DEVELOPER</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="resume">
+              <div class="container">
+                <div class="row">
+                  <div class="col-xl-6">
+                    <div class="box bb br">
+                      <h2>Skills Summary</h2>
+                      <div class="skills">
+                        <span>HTML5</span><span>CSS3</span><span>SASS</span><span>LESS</span><span>Javascript</span><span>jQuery</span><span>Typescript</span>
+                        <span>Angular</span><span>React</span><br><span>Unit Testing</span><span>Bootstrap</span><span>Wordpress</span><span>Responsive Design</span>
+                        <span>Gulp</span><span>Node.js</span><span>PHP</span><span>Rest API</span>
+                        <span>MySQL</span><span>Git</span><span>SASS</span><span>PS</span><span>Ai</span><span>Id</span>
+                        <span>Windows</span><span>Linux</span><span>Mac</span><br>
+                        <span>Agile SD</span><span>Jira</span>
+                      </div>
+                    </div>
+                    <div class="box bb br">
+                      <h2>Academic Background</h2>
+                      <div class="academic">
+                        <div class="wrap">
+                          <h3>Masters Degree</h3>
+                          <span>2017 - 2021</span>
+                          <p><b>Information Technology</b></p>
+                          <p>Faculty of Electronic Engineering</p>
+                          <p>University of Niš</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Bachelor Degree</h3>
+                          <span>2009 - 2016</span>
+                          <p><b>Computer Science</b></p>
+                          <p>Faculty of Electronic Engineering</p>
+                          <p>University of Niš</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="box bb br">
+                      <h2>Profile</h2>
+                      <div class="profile">
+                        <div class="wrap">
+                          <ul>
+                            <li>persistent</li><li>tolerant</li><li>careful</li><li>patient</li><li>emphatic</li><br>
+                            <li>person of trust</li><li>organized</li><li>hard-working person</li><li>accurate</li><br>
+                            <li>dedicated to work</li><li>analytical</li><li>pragmatical</li><li>creative</li><br>
+                            <li>self-motivated</li><li>possesing leadership skills</li><li>intuitive</li><br>
+                            <li>possessing conflict management skills</li><li>open for indipendent and team work</li><li>working well under pressure and adhering to strict deadlines</li><br>
+                            <li>always enthusiastic to learn and undertake new challenges</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="box bb br">
+                      <h2>Languages</h2>
+                      <div class="languages">
+                        <div class="wrap">
+                          <div class="table-responsive">
+                            <table class="table table-sm table-borderless">
+                              <thead>
+                                <tr>
+                                  <th scope="col">Languages</th>
+                                  <th scope="col">Understanding</th>
+                                  <th scope="col">Speaking</th>
+                                  <th scope="col">Writing</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td class="text-left !important">English</td>
+                                  <td>B2</td>
+                                  <td>B2</td>
+                                  <td>B2</td>
+                                </tr>
+                                <tr>
+                                  <td>German</td>
+                                  <td>A2</td>
+                                  <td>A2</td>
+                                  <td>A2</td>
+                                </tr>
+                                <tr>
+                                  <td>Italian</td>
+                                  <td>A1</td>
+                                  <td>A1</td>
+                                  <td>A1</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="box br">
+                      <h2>Other Interests</h2>
+                      <div class="interests">
+                        <div class="wrap">
+                          <div>
+                            <span>Reading</span><span>Writing</span>
+                          </div>
+                          <div>
+                            <span>Painting</span><span>Photography</span>
+                          </div>
+                          <div>
+                            <span>Travelling</span><span>Swimming</span>
+                          </div>
+                          <div>
+                            <span>Fitness</span><span>Dancing</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-xl-6">
+                    <div class="box bb">
+                      <h2>Employment History</h2>
+                      <div class="employment">
+                        <div class="wrap">
+                          <h3>Web Developer</h3>
+                          <span>Oct 2019 - Present</span>
+                          <p>Kinto Join Ltd (Toyota sub company)</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Web Developer Intern</h3>
+                          <span>Nov 2018 - Sep 2019</span>
+                          <p>e-hoi GmbH, Germany</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Web Developer and QA Manual Tester</h3>
+                          <span>Feb 2017 - Nov 2019</span>
+                          <p>Canic Interactive LLC, Serbia</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Course Designer and Content Writer</h3>
+                          <span>Jun 2017 - Jan 2019</span>
+                          <p>Nobel Coaching, Freelance work</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Web Developer Intern</h3>
+                          <span>Sep 2015 - Dec 2015</span>
+                          <p>Computer Center at Faculty of Medicine, Serbia</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Dance Teacher, Zumba Coach</h3>
+                          <span>Jan 2013 - Dec 2015</span>
+                          <p>DC "Step", DC "Step by step", DC "Vračar", Serbia</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="box">
+                      <h2>Other Proffesional Experience</h2>
+                      <div class="employment">
+                        <div class="wrap">
+                          <h3>Job Club Training</h3>
+                          <span>2017</span>
+                          <p>National Employment Service, Serbia</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Volunteer on iSerbia.rs</h3>
+                          <span>2016</span>
+                          <p>Translator (English to Serbian)</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Technic in the Dance Sport WDC</h3>
+                          <span>2015</span>
+                          <p>Workshop, Italy</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Lugano Goes Mobile</h3>
+                          <span>2011</span>
+                          <p>Workshop for Android applications, Switzerland</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Member of EESTEC Organization</h3>
+                          <span>2009 - 2011</span>
+                          <p>Position of cashier, Serbia</p>
+                        </div>
+                        <div class="wrap">
+                          <h3>Volunteer on Project "Balkan Footnote"</h3>
+                          <span>2006</span>
+                          <p>Technic support, Bosnia and Herzegovina</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="contact bt bb">
+                <div class="container">
+                  <div class="row justify-content-md-center">
+                    <div class="col-md-4 text-center">
+                      <a href="mailto: dejana.jankovic@outlook.com"><i class="fas fa-envelope"></i></a>
+                      <a href="https://www.linkedin.com/in/dejanajankovic"><i class="fab fa-linkedin"></i></a>
+                      <a href="https://github.com/dejanajan"><i class="fab fa-github-square"></i></a>
+                    </div>
+                  </div>
+                  <div class="row justify-content-md-center">
+                    <div class="col-md-4 text-center pt-2">
+                        <img src="assets/img/signature-bg.png"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+          <footer>
+            <div class="wrap">
+              <div class="container">
+                <div class="row">
+                  <div class="col-10 col-md-6">
+                    <p>&copy; Copyright <?php echo date("Y"); ?>. All right reserved. Created by #DJTeam</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a href="#top"><i class="fas fa-arrow-up"></i></a>
+          </footer>
+      </div>
+    </div>
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
